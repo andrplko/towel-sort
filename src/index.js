3f3356f16aabdeef000801991b70daa885ee6dc2
Andrei Paleshka <andrepoleshko@gmail.com>
2023-10-17 17:53:41 +0200

    refactor: improve function implementation

--- src/index.js
+++ src/index.js
@@ -1,29 +1,15 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  if (matrix === undefined || matrix.length === 0) {
-    return [];
-  }
-
-  else if (matrix.length === 2) {
-    let arr1 = matrix[0].sort((a,b) => a - b);
-    let arr2 = matrix[1].sort((a,b) => b - a);
-    return arr1.concat(arr2);
-  }
-
-  else if (matrix.length === 3) {
-    let arr1 = matrix[0].sort((a,b) => a - b);
-    let arr2 = matrix[1].sort((a,b) => b - a);
-    let arr3 = matrix[2].sort((a,b) => a - b);
-    return arr1.concat(arr2, arr3);
-  }
-
-  else if (matrix.length === 4) {
-    let arr1 = matrix[0].sort((a,b) => a - b);
-    let arr2 = matrix[1].sort((a,b) => b - a);
-    let arr3 = matrix[2].sort((a,b) => a - b);
-    let arr4 = matrix[3].sort((a,b) => b - a);
-    return arr1.concat(arr2, arr3, arr4);
-  }
+module.exports = function towelSort(matrix) {
+  if (!matrix || !matrix.length) return [];
+  return matrix.reduce((acc, cur, i) => {
+    if (i % 2 !== 0) {
+      acc.push(...cur.reverse())
+    } else {
+      acc.push(...cur)
+    }
+    return acc;
+  }, [])
 }
+
